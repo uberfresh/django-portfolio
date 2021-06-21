1ab3db7b4e9298ec4029eb4c5a3b8b04ffaef5e1
uberfresh <99hanbatu@windowslive.com>
2021-06-21 04:05:58 +0300

    Heroku

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4oc%7(!sgqn_=l3f_q2j78ilb=2twnba)ue-%iklm804*3(&s3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','']
 
 
 # Application definition
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- mywebsite/mywebsite/urls.py
+++ mywebsite/mywebsite/urls.py
@@ -3,6 +3,8 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 from blog import views
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,5 +15,8 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
 
     path('ckeditor',include('ckeditor_uploader.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ] + static(settings.MEDIA_URL,document_root =settings.MEDIA_ROOT)
