cf030ab9f53c7026db489a857e48fd4e92f1dc0f
uberfresh <99hanbatu@windowslive.com>
2021-06-21 04:36:27 +0300

    screenshots added

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4oc%7(!sgqn_=l3f_q2j78ilb=2twnba)ue-%iklm804*3(&s3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','batuhanbaspinar-portfolio.herokuapp.com']
 
