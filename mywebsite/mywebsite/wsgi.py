91302d32bc1cc98206b035fe0ec5756ef859c33b
uberfresh <99hanbatu@windowslive.com>
2021-07-14 01:59:39 +0300

    heroku

--- mywebsite/mywebsite/wsgi.py
+++ mywebsite/mywebsite/wsgi.py
@@ -6,10 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
-
+import sys
 import os
 
 from django.core.wsgi import get_wsgi_application
+path = 'C:/Users/99han/OneDrive/Belgeler/GitHub/djangoPortfolio'
+if path not in sys.path:
+	sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
 
